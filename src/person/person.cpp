8ee14cabfc4df14e5920e37583115e743dde5ec4
Hoani Bryson <Hoani.Bryson@gmail.com>
2020-06-14 19:42:42 +1200

    Resolve annoying localtime portability issue

--- src/person/person.cpp
+++ src/person/person.cpp
@@ -1,24 +1,25 @@
 #include "person.hpp"
 
 
-Person::Person(IZodiac * zodiac, std::string name) : _birth(std::tm()), _name(name), _zodiac(zodiac) {
+Person::Person(IZodiac * zodiac, std::string name) : _birth(nullptr), _name(name), _zodiac(zodiac) {
   std::time_t now = time(0);
-  localtime_s(&_birth, &now);
+  _birth = localtime(&now);
 }
 
 Person::~Person() {
+  delete _birth;
 }
 
 void Person::SetBirthday(int day, int month, int year) {
-  _birth.tm_year = year - 1900;
+  _birth->tm_year = year - 1900;
   if (month > 0 && month <= 12) {
-    _birth.tm_mon = month - 1;
+    _birth->tm_mon = month - 1;
   } 
-  _birth.tm_mday = day;
+  _birth->tm_mday = day;
 }
 
 std::string Person::ToString() {
   char buffer[80];
-  std::strftime(buffer, sizeof(buffer)," born in %B %Y", &_birth);
-  return _name + std::string(buffer) + "\n" + _zodiac->GetString(&_birth);
+  std::strftime(buffer, sizeof(buffer)," born in %B %Y", _birth);
+  return _name + std::string(buffer) + "\n" + _zodiac->GetString(_birth);
 }
